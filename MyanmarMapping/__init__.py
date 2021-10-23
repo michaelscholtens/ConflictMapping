f1a4e18fbd528be1ce1d602f3c900e08c94ec6c8
Michael Scholtens <48947001+michaelscholtens@users.noreply.github.com>
2021-10-22 23:48:08 -0400

    Comments and Actor table support

--- MyanmarMapping/__init__.py
+++ MyanmarMapping/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 import pandas as pd
 import requests as rq
 import numpy as np
-import math
+import time
 
 from urllib.parse import quote_plus
 from sqlalchemy import create_engine, event
@@ -17,6 +17,7 @@ def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
+    #Function used to ensure data types are stored correctly on ingestion to the SQL database.
     def sqlcol(dfparam):    
     
         dtypedict = {}
@@ -35,17 +36,20 @@ def main(mytimer: func.TimerRequest) -> None:
 
         return dtypedict
 
-    #Connection String to Target Database.
+    #Connection String to target Database.
     conn ='Driver={ODBC Driver 17 for SQL Server};Server=tcp:myanmarmapping.database.windows.net,1433;Database=myanmarMapping;Uid=tccuser;Pwd=2Legit2Quit;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
     quoted = quote_plus(conn)
     engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted), fast_executemany = True)
 
+    # For loop used to ensure that connection timeouts when connecting to elastic pools do not cause a failure. Could be refactored to try/except logic, but this is fully functional.
     for i in range(2):
         try:
+            #Table name for main conlfict events table 
             table_name = 'acledEvents'
-
+            #ACLED key for pulling data
             acledKey = 'ZFU2-Xr9dvypqlvEKOHa'
 
+            #API request for ACLED data. See documentation at https://acleddata.com/resources/general-guides/
             data = rq.get('https://api.acleddata.com/acled/read?key=' + acledKey + '&email=michael.scholtens@cartercenter.org&country=Myanmar&timestamp>=2021-02-01&limit=0')
             data = data.json()
 
@@ -53,8 +57,11 @@ def main(mytimer: func.TimerRequest) -> None:
 
             types = sqlcol(acledEvents)
 
+            #Write data to database
             acledEvents.to_sql(table_name, engine, index=False, if_exists='replace', schema='dbo', chunksize = 1000, dtype = types)
 
+
+            #This next section of code produces the adjecency list for the 'Shape of Conflict' page of the Power Bi Dashboard.
             actorMap = pd.DataFrame()
             id = []
             s = []
@@ -82,9 +89,30 @@ def main(mytimer: func.TimerRequest) -> None:
 
             actorMap['date'] =  pd.to_datetime(actorMap['date'], format='%Y-%m-%d')
 
+            #Write the adjacency list to the database
             actorMap.to_sql(table_name, engine, index=False, if_exists='replace', schema='dbo', chunksize = 1000, dtype = types)
 
+            table_name = 'actors'
+
+            #This section of code reads the actors table from the database and identifies new actors from the new conflcit events and adds them to the table with the class "Not Yet Classified"
+            actors = pd.read_sql(table_name, engine)
+
+            actorsClass = set(actors['Actors'])
+            actorsOld = set(acledEvents['actor1'])
+            actorsNew = list(actorsClass.symmetric_difference(actorsOld))
+
+            actorsComplete = pd.DataFrame(actorsNew, columns = ['Actors'])
+
+            actorsComplete['Classification'] = "Not Yet Classified"
+
+            actorsUpdate = actors.append(actorsComplete, ignore_index = True)
+            
+            #Write actors table to database
+            actorsUpdate.to_sql(table_name, engine, index=False, if_exists='replace', schema='dbo', chunksize = 1000)
+
+
         except: 
+            print("Failed")
             continue
 
     if mytimer.past_due:
